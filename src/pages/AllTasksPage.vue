82ec8a08223540a8a51a6033c93a5446efde3be6
Elvin Topalovic <166206476+elvi93@users.noreply.github.com>
2024-07-13 10:39:14 +0200

    Update AllTasksPage.vue

--- src/pages/AllTasksPage.vue
+++ src/pages/AllTasksPage.vue
@@ -77,7 +77,7 @@ function editTask(task) {
 // Utility function to compare two task objects
 function areTasksEqual(task1, task2) {
   return (
-    //task1.title === task2.title &&
+    task1.title === task2.title &&
     task1.description.title === task2.description.title &&
     task1.description.timeToBeCompleted === task2.description.timeToBeCompleted &&
     JSON.stringify(task1.description.extraInfoRequired) === JSON.stringify(task2.description.extraInfoRequired)
